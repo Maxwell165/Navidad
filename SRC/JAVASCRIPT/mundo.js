ad0c111aa1be966340da47c6dc36b22ccbbe07db
JeanFrancoRodriguez <jeanfranco.jfrl@gmail.com>
2024-11-20 12:03:19 -0500

    Se agrego funcionalidad a los pines para hacer zoom, se creo botones para regresar a la posicion inicial del mapa y botones para avanzar y retroceder en los pines del archivo mundo.js // mundo.html

--- SRC/JAVASCRIPT/mundo.js
+++ SRC/JAVASCRIPT/mundo.js
@@ -1,9 +1,11 @@
 class Marcador {
-    constructor(lat, lng, url, map) {
+    constructor(title,lat, lng, url, map) {
+        this.title = title;
         this.lat = lat;
         this.lng = lng;
         this.url = url;
         this.map = map;
+        this.marker = null
         this.crearMarcador();
     }
     crearMarcador() {
@@ -13,15 +15,46 @@ class Marcador {
         }
 
 
-        new google.maps.Marker({
+         this.marker = new google.maps.Marker({
+            title: this.title,
             position: { lat: this.lat, lng: this.lng },
             map: this.map,
-            icon: icono
+            icon: icono,
         })
     }
-
 }
 
+//EVENTOS PARA LOS MARCADORES
+class eventos {
+    constructor(marcadores,map){
+        this.marcadores = marcadores
+        this.map = map
+        this.enfocarMarcador()
+    }
+
+
+    enfocarMarcador(){
+        this.marcadores.forEach((marcador) => {
+        google.maps.event.addListener(marcador.marker,"click",()=>{
+            this.map.setCenter(marcador.marker.getPosition())
+            this.map.setZoom(4.5)
+            this.ma
+            })
+        })
+    }
+}
+
+// FUNCION BOTON MOSTRAR MAPA COMPLETO
+const zoomCompleto = (map) =>{
+    const planeta = document.getElementById("planeta")
+    planeta.addEventListener("click",()=>{
+    map.setCenter({ lat: 15, lng: 15 })
+    map.setZoom(2.8)
+    })
+}
+
+
+// FUNCION PARA CREAR MAPA PERSONALIZADO , CONSTANTES PARA LOS MARCADORES Y INICIAR INSTANCIAS Y FUNCIONES
 function initMap() {
     const map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 15, lng: 15 },  // Coordenadas del centro del mapa
@@ -33,27 +66,35 @@ function initMap() {
         zoomControl: false,  // Desactivar el control de zoom
         mapTypeControl: false,  // Desactivar el control de tipo de mapa
         streetViewControl: false,  // Desactivar el control de Street View
-        fullscreenControl: false,  // Desactivar el control de pantalla completa       
+        fullscreenControl: false,  // Desactivar el control de pantalla completa        
+        
     });
-    //INSTANCIAS PARA MARCAR PAISES AMERICANOS
-    const Peru = new Marcador(-10.595594465114734, -76.30662798071761, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const guatemala = new Marcador(15.78, -90.2, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const EEUU = new Marcador(  37.09024, -95.712891, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const brasil = new Marcador( -14.235004, -51.92528, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const canada = new Marcador( 56.130366, -106.3467, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const mexico = new Marcador( 19.432608, -99.13320, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const argentina = new Marcador ( -38.416097, -63.61, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    //INSTACIONES PARA MARCAR PAISES EUROPEOS, AFRICANOS, ASIATICOS y OCEANIA
-    const francia = new Marcador (46.569177329934845, 1.786342174972705, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map )
-    const rusia = new Marcador (61.52401, 105.3187, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const ghana = new Marcador ( 7.946527, -1.02319, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const australia = new Marcador ( -24.673317248276838, 135.38008901939668,"https://cdn-icons-png.flaticon.com/512/149/149059.png",map )
-    const china = new Marcador ( 35.861660, 104.19539, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const filipinas = new Marcador ( 13.495204820573125, 124.19399463819018, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const finlandia = new Marcador( 61.924110, 25.74815, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const nuevazelanda = new Marcador(-43.262155080828116, 170.776025396811, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
-    const reinounido = new Marcador (55.00598577080574, -2.8432026859699615, "https://cdn-icons-png.flaticon.com/512/149/149059.png",map)
+    // ARRAY DE OBJETOS DE CADA PAIS
+    const paises = [
+        { "nombre": "Peru", "latitud": -10.595594465114734, "longitud": -76.30662798071761, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Guatemala", "latitud": 15.78, "longitud": -90.2, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "EEUU", "latitud": 37.09024, "longitud": -95.712891, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Brasil", "latitud": -14.235004, "longitud": -51.92528, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Canadá", "latitud": 56.130366, "longitud": -106.3467, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "México", "latitud": 19.432608, "longitud": -99.13320, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Argentina", "latitud": -38.416097, "longitud": -63.61, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Francia", "latitud": 46.569177329934845, "longitud": 1.786342174972705, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Rusia", "latitud": 61.52401, "longitud": 105.3187, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Ghana", "latitud": 7.946527, "longitud": -1.02319, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Australia", "latitud": -24.673317248276838, "longitud": 135.38008901939668, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "China", "latitud": 35.861660, "longitud": 104.19539, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Filipinas", "latitud": 13.495204820573125, "longitud": 124.19399463819018, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Finlandia", "latitud": 61.924110, "longitud": 25.74815, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Nueva Zelanda", "latitud": -43.262155080828116, "longitud": 170.776025396811, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Reino Unido", "latitud": 55.00598577080574, "longitud": -2.8432026859699615, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+    ];
+    // INTANCIA CON UN MAP PARA CREAR UN MARCADOR
+      const Paises = paises.map((pais) => 
+        new Marcador(pais.nombre, pais.latitud, pais.longitud, pais.icono, map))
+    // INSTANCIA PARA EL ENVENTO DE ENFOQUE DE MARCADOR
+      const evento = new eventos(Paises,map)
+      evento.enfocarMarcador()
+    //PANTALLA COMPLETA FUNCION BOTON
+      zoomCompleto(map)
 }
-
-
 
