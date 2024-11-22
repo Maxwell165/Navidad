5f5d2cfc1bccf3559f6ae51af1aa3ce00aaa77a0
JeanFrancoRodriguez <jeanfranco.jfrl@gmail.com>
2024-11-22 18:16:08 -0500

    Se añadio un cuadro descriptivo mas su respectiva imagen en cada pin, mostrara informacion de acuerdo a que pais se escoja con la ayuda de la cclase enfocarMarcador()

--- SRC/JAVASCRIPT/mundo.js
+++ SRC/JAVASCRIPT/mundo.js
@@ -1,5 +1,6 @@
+
 class Marcador {
-    constructor(title,lat, lng, url, map) {
+    constructor(title, lat, lng, url, map) {
         this.title = title;
         this.lat = lat;
         this.lng = lng;
@@ -10,47 +11,107 @@ class Marcador {
     }
     crearMarcador() {
         const icono = {
-            url : this.url,
-            scaledSize: new google.maps.Size(40, 40)
+            url: this.url,
+            scaledSize: new google.maps.Size(100, 100),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(50, 50)
+
         }
 
 
-         this.marker = new google.maps.Marker({
+        this.marker = new google.maps.Marker({
             title: this.title,
             position: { lat: this.lat, lng: this.lng },
             map: this.map,
             icon: icono,
         })
     }
+
 }
 
 //EVENTOS PARA LOS MARCADORES
 class eventos {
-    constructor(marcadores,map){
+    constructor(marcadores, map) {
         this.marcadores = marcadores
         this.map = map
+        this.indice = 0
+        this.resultado = null
+        this.marcadorenfocado = false
         this.enfocarMarcador()
+        this.moverMarcador()
     }
 
-
-    enfocarMarcador(){
+    //METODO PARA ENFOCAR EL PIN
+    enfocarMarcador() {
+        console.log(this.marcadorenfocado)
         this.marcadores.forEach((marcador) => {
-        google.maps.event.addListener(marcador.marker,"click",()=>{
-            this.map.setCenter(marcador.marker.getPosition())
-            this.map.setZoom(4.5)
-            this.ma
+            google.maps.event.addListener(marcador.marker, "click", () => {
+                this.map.setCenter(marcador.marker.getPosition())
+                this.map.setZoom(4.5)
+            
+                this.resultado = modalpaises.find((elemento) =>
+                    elemento.nombre.toLowerCase() === marcador.title.toLowerCase());
+                    InsertarPaises(this.resultado)
+                    InsertarImagen(this.resultado)
+                    this.indice = modalpaises.indexOf(this.resultado)
+                    this.marcadorenfocado = true
+                console.log(this.resultado)
+                console.log(this.indice)
             })
         })
+
+    }
+
+
+    moverMarcador() {
+        //METODO PARA AVANZAR CON LAS FLECHAS
+        const atras = document.getElementById("atras")
+        const delante = document.getElementById("delante")
+                // EVENTO PARA AVANZAR CON EL BOTON 
+            atras.addEventListener("click", () => {
+                try {
+
+                        let newindice = this.indice - 1
+                        console.log(newindice)
+
+                    
+
+
+
+                }
+                catch {
+                    console.error("Error al atras")
+                }
+            })
+            // EVENTO PARA AVANZAR CON EL BOTON 
+            delante.addEventListener("click", () => {
+                try {
+                    
+                    
+                }
+                catch {
+                    console.error("Error al delante")
+                }
+
+            })
+
+
+
+
+        
     }
 }
-
-// FUNCION BOTON MOSTRAR MAPA COMPLETO
-const zoomCompleto = (map) =>{
+// METODO PAARA MOSTRAR  MAPA COMPLETO
+const zoomCompleto = (map) => {
     const planeta = document.getElementById("planeta")
-    planeta.addEventListener("click",()=>{
-    map.setCenter({ lat: 15, lng: 15 })
-    map.setZoom(2.8)
+    planeta.addEventListener("click", () => {
+        map.setCenter({ lat: 15, lng: 15 })
+        map.setZoom(2.5)
+        const contenedorPaises = document.getElementById("paises")
+        contenedorPaises.innerHTML = ""
+
     })
+
 }
 
 
@@ -58,43 +119,48 @@ const zoomCompleto = (map) =>{
 function initMap() {
     const map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 15, lng: 15 },  // Coordenadas del centro del mapa
-        zoom: 2.8,  // Nivel de zoom
         mapId: '4f4beacbe7b0addc',
+        zoom: 2.5,
         disableDefaultUI: true,  // Desactivar todos los controles predeterminados
         draggable: false,  // Desactivar la capacidad de arrastrar el mapa
         scrollwheel: false,  // Desactivar el zoom con la rueda del ratón
         zoomControl: false,  // Desactivar el control de zoom
         mapTypeControl: false,  // Desactivar el control de tipo de mapa
         streetViewControl: false,  // Desactivar el control de Street View
-        fullscreenControl: false,  // Desactivar el control de pantalla completa        
-        
+        fullscreenControl: false,  // Desactivar el control de pantalla completa          
     });
+
     // ARRAY DE OBJETOS DE CADA PAIS
     const paises = [
-        { "nombre": "Peru", "latitud": -10.595594465114734, "longitud": -76.30662798071761, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Guatemala", "latitud": 15.78, "longitud": -90.2, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "EEUU", "latitud": 37.09024, "longitud": -95.712891, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Brasil", "latitud": -14.235004, "longitud": -51.92528, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Canadá", "latitud": 56.130366, "longitud": -106.3467, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "México", "latitud": 19.432608, "longitud": -99.13320, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Argentina", "latitud": -38.416097, "longitud": -63.61, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Francia", "latitud": 46.569177329934845, "longitud": 1.786342174972705, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Rusia", "latitud": 61.52401, "longitud": 105.3187, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Ghana", "latitud": 7.946527, "longitud": -1.02319, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Australia", "latitud": -24.673317248276838, "longitud": 135.38008901939668, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "China", "latitud": 35.861660, "longitud": 104.19539, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Filipinas", "latitud": 13.495204820573125, "longitud": 124.19399463819018, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Finlandia", "latitud": 61.924110, "longitud": 25.74815, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Nueva Zelanda", "latitud": -43.262155080828116, "longitud": 170.776025396811, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
-        { "nombre": "Reino Unido", "latitud": 55.00598577080574, "longitud": -2.8432026859699615, "icono": "https://cdn-icons-png.flaticon.com/512/149/149059.png" },
+        { "nombre": "Peru", "latitud": -10.595594465114734, "longitud": -76.30662798071761, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-rojo.svg" },
+        { "nombre": "Guatemala", "latitud": 15.78, "longitud": -90.2, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-celeste.svg" },
+        { "nombre": "EEUU", "latitud": 37.09024, "longitud": -95.712891, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-marron.svg" },
+        { "nombre": "Brasil", "latitud": -14.235004, "longitud": -51.92528, "icono": "/SRC/IMAGE/MUNDO/SVG/pin.amarillo.svg" },
+        { "nombre": "Canada", "latitud": 45.130366, "longitud": -106.3467, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-naranja.svg" },
+        { "nombre": "Mexico", "latitud": 25.432608, "longitud": -105.13320, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-rosado.svg" },
+        { "nombre": "Argentina", "latitud": -38.416097, "longitud": -63.61, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-verde.svg" },
+        { "nombre": "Francia", "latitud": 46.569177329934845, "longitud": 1.786342174972705, "icono": "/SRC/IMAGE/MUNDO/SVG/pin.amarillo.svg" },
+        { "nombre": "Rusia", "latitud": 61.52401, "longitud": 80.3187, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-rojo.svg" },
+        { "nombre": "Ghana", "latitud": 7.946527, "longitud": -1.02319, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-azul-oscuro.svg" },
+        { "nombre": "Australia", "latitud": -24.673317248276838, "longitud": 135.38008901939668, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-verde.svg" },
+        { "nombre": "Corea del sur", "latitud": 36.20955300864592, "longitud": 127.81755223789501, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-rojo.svg" },
+        { "nombre": "Filipinas", "latitud": 13.495204820573125, "longitud": 124.19399463819018, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-azul-oscuro.svg" },
+        { "nombre": "Finlandia", "latitud": 61.924110, "longitud": 25.74815, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-rosado.svg" },
+        { "nombre": "Nueva Zelanda", "latitud": -43.262155080828116, "longitud": 170.776025396811, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-celeste.svg" },
+        { "nombre": "Reino Unido", "latitud": 55.00598577080574, "longitud": -2.8432026859699615, "icono": "/SRC/IMAGE/MUNDO/SVG/pin-marron.svg" },
     ];
     // INTANCIA CON UN MAP PARA CREAR UN MARCADOR
-      const Paises = paises.map((pais) => 
+    const Paises = paises.map((pais) =>
         new Marcador(pais.nombre, pais.latitud, pais.longitud, pais.icono, map))
     // INSTANCIA PARA EL ENVENTO DE ENFOQUE DE MARCADOR
-      const evento = new eventos(Paises,map)
-      evento.enfocarMarcador()
+    new eventos(Paises, map)
     //PANTALLA COMPLETA FUNCION BOTON
-      zoomCompleto(map)
+    zoomCompleto(map)
+    // AÑADIR INFORMACION A CADA PIN
 }
+
+
+
+
+
 
---
+++ SRC/JAVASCRIPT/paises.js
@@ -0,0 +1,132 @@
+let modalpaises = [
+    {
+        nombre: "Brasil",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/br.png",
+        descripcion: "Los festejos de Navidad comienzan cerca de la medianoche y suelen incluir grandes cenas en familia, regalos y la celebración de la 'Misa de Gallo' en las iglesias. A pesar del clima cálido y tropical del verano, es frecuente el uso de decoración relacionada con el invierno y la nieve.",
+    },
+    {
+        nombre: "Peru",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/pe.png", 
+        descripcion: "En Perú, la Navidad se caracteriza por reuniones familiares en la noche del 24 de diciembre, con una cena tradicional que incluye pavo, panetón y chocolate caliente, a pesar del clima veraniego. Un dato curioso es el 'chocolatón', donde comunidades comparten chocolate y panetón con los más necesitados. También destacan ferias como el Santurantikuy en Cusco, donde se venden artesanías y figuras para nacimientos, resaltando la tradición andina en la festividad.",
+    },
+    {
+        nombre: "Argentina",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/ar.png", 
+        descripcion: "En Argentina, la Navidad comienza exactamente a la medianoche. Para dar inicio a esta festividad, muchas familias se reúnen a celebrar, se hacen regalos y encienden fuegos artificiales a primeras horas de la madrugada.",
+
+    },
+    {
+        nombre: "Guatemala",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/gt.png",
+        descripcion: "Los guatemaltecos celebran la Navidad con festejos y bailes. Usan sombreros denominados 'Puritina', comen tamales al estilo guatemalteco, arrojan petardos a la medianoche y bailan distintos estilos hasta el amanecer.",
+
+    },
+    {
+        nombre: "Mexico",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/mx.png", 
+        descripcion: "En México, niños de todas las edades rompen piñatas para Navidad. Adultos y niños por igual se vendan los ojos, dan vueltas y, con el golpe de un bastón, intentan abrir las piñatas colgantes con forma de estrella. Cuando las abren, caen nueces, frutas y golosinas para alegría de todos.",
+
+    },
+    {
+        nombre: "EEUU",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/us.png",
+        descripcion: "En Estados Unidos, se cuelgan calcetines en Nochebuena. Según la leyenda, San Nicolás quiso ayudar a una familia y, para sorprenderla, bajó por la chimenea y colocó bolsas de oro en unos calcetines que colgaban allí. Desde entonces, San Nicolás pasó a ser Santa Claus, famoso por sus regalos.",
+
+    },
+    {
+        nombre: "Canada",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/ca.png", 
+        descripcion: "Los niños canadienses suelen festejar una Navidad blanca, con mucha nieve. Se cubren con bufandas y gorros de lana para jugar en la nieve, patinar sobre hielo y andar en trineo. En Nochebuena, dejan leche y galletas para Santa junto a la chimenea y, a veces, una zanahoria para los renos.",
+
+    },
+    {
+        nombre: "Ghana",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/gh.png", 
+        descripcion: "La Navidad en Ghana incluye a toda la familia. Los adultos se disfrazan y reparten golosinas y dulces en todo el barrio. Los niños decoran sus casas con arreglos navideños de papel y dibujos que hacen en la escuela.",
+
+    },
+    {
+        nombre: "Francia",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/fr.png", 
+        descripcion: "En Francia, la tradición consiste en que los niños coloquen sus zapatos junto a la chimenea para esperar la llegada de Santa. Si se portaron bien durante el año, Santa llenará los zapatos con regalos.",
+
+    },
+    {
+        nombre: "Reino Unido",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/uk.png", 
+        descripcion: "Los festejos de Navidad comienzan cerca de la medianoche y suelen incluir grandes cenas en familia, regalos y la celebración de la 'Misa de Gallo' en las iglesias. A pesar del clima cálido y tropical del verano, es frecuente el uso de decoración relacionada con el invierno y la nieve.",
+
+    },
+    {
+        nombre: "Rusia",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/ru.png", 
+        descripcion: "En el Reino Unido, entusiastas de todas las edades se divierten con un artículo típico llamado “Christmas cracker”, que consiste en un tubo envuelto en papel de colores, con tiras en cada extremo. Cuando se jalan, el tubo estalla con un fuerte sonido y salen sombreros de papel, juguetes, bromas y premios.",
+
+    },
+    {
+        nombre: "Corea del sur",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/kr.png",
+        descripcion: "En Corea del Sur, la Navidad es un feriado nacional. También se considera un feriado romántico, ya que muchas parejas tienen citas en la ciudad, donde las calles y tiendas están adornadas con luces resplandecientes.",
+    },
+    {
+        nombre: "Filipinas",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/ph.png", 
+        descripcion: "Filipinas es famosa por tener la temporada de Navidad más larga del mundo. Abarca cuatro meses: septiembre, octubre, noviembre y diciembre. Los villancicos navideños suelen comenzar a escucharse desde principios de septiembre.",
+    },
+    {
+        nombre: "Australia",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/au.png", 
+        descripcion: "Su ubicación en el hemisferio sur hace que Australia reciba la Navidad en verano, en el mes de diciembre. En lugar de comer chocolate y decorar con cascabeles, muchos australianos celebran esta festividad en la playa con un almuerzo de camarones y algunas partidas de cricket.",
+    },
+    {
+        nombre: "Finlandia",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/fi.png", 
+        descripcion: "Los finlandeses creen que Santa Claus ('Joulupukki') vive en la zona norte del país. En Korvatunturi, para ser precisos. Si lo visitas, podrías descubrir su taller secreto donde los duendes trabajan arduamente o podrías encontrar renos paseando por los bosques de pinos.",
+    },
+    {
+        nombre: "Nueva Zelanda",
+        imagen: "/SRC/IMAGE/MUNDO/COS-IMG/nz.png", 
+        descripcion: "En Nueva Zelanda, amigos y familiares hacen una parrillada para Navidad. Una de las comidas típicas es la carne de cordero, que se cocina en un 'hangi' (un hoyo en el suelo o un horno de barro) y el postre preferido es el 'pavlova': merengue cubierto de frutas frescas, como frutillas y kiwi.",
+    },
+];
+function InsertarImagen(pais){
+    try{
+        const contenedorPaises = document.getElementById("paises")
+        const img = document.createElement("img")
+        img.className = "img-pais"
+        img.src = pais.imagen
+        contenedorPaises.appendChild(img)
+
+    }
+    catch{
+        console.error("Error al Insertar imagenes")
+    }
+}
+
+function InsertarPaises(pais) {
+    try{
+        const contenedorPaises = document.getElementById("paises")
+        contenedorPaises.innerHTML = ""
+        const contenedor = document.createElement("div")
+        const titulo = document.createElement("h2")
+        const separador = document.createElement("span")
+        const descripcion = document.createElement("p")
+        
+
+        
+        contenedor.className = "card"
+        titulo.textContent = pais.nombre
+        separador.textContent = "----------------------------------------------"
+        descripcion.textContent = pais.descripcion
+
+        contenedor.appendChild(titulo)
+        contenedor.appendChild(separador)
+        contenedor.appendChild(descripcion)
+        contenedorPaises.appendChild(contenedor)
+    }
+    catch{
+        console.error("Error al Insertar los paises")
+    }
+        
+    }
+
